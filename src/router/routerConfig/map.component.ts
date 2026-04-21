18355898c11955034762f113df1dde8e5c3b5efb
lixinzz <xin.li@pkucollege.com>
2026-04-21 15:43:54 +0800

    代码提交

--- src/router/routerConfig/map.component.ts
+++ src/router/routerConfig/map.component.ts
@@ -3,27 +3,27 @@ import { RouteRecordRaw } from 'vue-router'
 // 数据展示组件路由
 export const mapComponent: RouteRecordRaw[] = [
   {
-    path: 'amap',
+    path: '/map/amap',
     meta: { title: 'amap 高德地图' },
     component: () => import('../../docs/map/amap/README.md')
   },
   {
-    path: 'bmap',
+    path: '/map/bmap',
     meta: { title: 'bmap 百度地图' },
     component: () => import('../../docs/map/bmap/README.md')
   },
   {
-    path: 'tmap',
+    path: '/map/tmap',
     meta: { title: 'tmap 腾讯地图' },
     component: () => import('../../docs/map/tmap/README.md')
   },
   {
-    path: 'smap',
+    path: '/map/smap',
     meta: { title: 'smap 天地图' },
     component: () => import('../../docs/map/smap/README.md')
   },
   {
-    path: 'omap',
+    path: '/map/omap',
     meta: { title: 'omap openlayer地图' },
     component: () => import('../../docs/map/omap/README.md')
   },
