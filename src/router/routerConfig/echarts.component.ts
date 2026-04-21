d05f612cedac6c8b783bef8b18c3584466e2c6af
lixinzz <xin.li@pkucollege.com>
2026-04-21 15:58:48 +0800

    代码提交

--- src/router/routerConfig/echarts.component.ts
+++ src/router/routerConfig/echarts.component.ts
@@ -3,27 +3,27 @@ import { RouteRecordRaw } from 'vue-router'
 // 数据展示组件路由
 export const echartComponent: RouteRecordRaw[] = [
   {
-    path: 'linechart',
+    path: 'charts/linechart',
     meta: { title: '折线图' },
     component: () => import('../../docs/charts/linechart/README.md')
   },
   {
-    path: 'barchart',
+    path: 'charts/barchart',
     meta: { title: '柱状图' },
     component: () => import('../../docs/charts/barchart/README.md')
   },
   {
-    path: 'piechart',
+    path: 'charts/piechart',
     meta: { title: '饼图' },
     component: () => import('../../docs/charts/piechart/README.md')
   },
   {
-    path: 'linebarchart',
+    path: 'charts/linebarchart',
     meta: { title: '折线柱状图' },
     component: () => import('../../docs/charts/linebarchart/README.md')
   },
   {
-    path: 'mapchart',
+    path: 'charts/mapchart',
     meta: { title: '地图' },
     component: () => import('../../docs/charts/mapchart/README.md')
   },
