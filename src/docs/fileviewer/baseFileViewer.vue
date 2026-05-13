3558358923051330e5de6254b1aa651b064463fc
lixinzz <xin.li@pkucollege.com>
2026-05-13 18:01:03 +0800

    新增提交

--- src/docs/fileviewer/baseFileViewer.vue
+++ src/docs/fileviewer/baseFileViewer.vue
@@ -1,34 +1,34 @@
 <template>
   <h4>pdf预览</h4>
   <zh-fileviewer style="width: 100%; height: 600px;"
-      url="/demo.pdf"
-      fileType="pdf"
-      :initialPage="1"
-      :initialScale="1.5"
+                 url="/demo.pdf"
+                 fileType="pdf"
+                 :initialPage="1"
+                 :initialScale="1.5"
   />
 
-  <el-divider />
+  <el-divider/>
 
   <h4>ofd预览</h4>
   <zh-fileviewer style="width: 100%; height: 600px;"
-      url="/test.ofd"
-      fileType="ofd"
+                 url="/test.ofd"
+                 fileType="ofd"
   />
 
-  <el-divider />
+  <el-divider/>
 
   <h4>word预览</h4>
   <zh-fileviewer style="width: 100%; height: 600px;"
-      url="/demo.docx"
-      fileType="word"
+                 url="/demo.docx"
+                 fileType="word"
   />
 
-  <el-divider />
+  <el-divider/>
 
   <h4>excel预览</h4>
   <zh-fileviewer style="width: 100%; height: 600px;"
-      url="/demo.et"
-      fileType="excel"
+                 url="/demo.et"
+                 fileType="excel"
   />
 </template>
 
